11c537ee82879df0374482ba85307799956967d4
Hector Saravia <hector.saravia@mail.udp.cl>
2021-05-22 22:57:00 -0400

    Actualizadas dependencias añadiendo Google apis

--- project_foro/src/App.vue
+++ project_foro/src/App.vue
@@ -1,15 +1,34 @@
 <template>
+<div>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GoogleLogin :params="params" :renderParams="renderParams" :onSuccess="onSuccess" :onFailure="onFailure"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import GoogleLogin from 'vue-google-login';
 
 export default {
   name: 'App',
+  data() {
+    return {
+    // client_id is the only required property but you can add several more params, full list down bellow on the Auth api section
+    params: {
+      client_id: "xxxxxx"
+    },
+    // only needed if you want to render the button with the google ui
+    renderParams: {
+      width: 250,
+      height: 50,
+      longtitle: true
+      }
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    GoogleLogin,
   }
 }
 </script>
