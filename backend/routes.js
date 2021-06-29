f6c7a4aac5e4e16d8e6300b15a13690d8a78324d
Hector Saravia <hector.saravia@mail.udp.cl>
2021-06-29 01:12:41 -0400

    Añadidas dos rutas: -Añadida ruta para registrar -Añadida ruta para testeos

--- backend/routes.js
+++ backend/routes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 
-const queries = require('./database/queries')
+const queries = require('./database/queries');
 
 router.get('/', (req, res) => {
  queries.getTables((err, results) => {
@@ -14,5 +14,23 @@ router.get('/', (req, res) => {
   };
  });
 });
+
+router.post('/register', (req, res) => {
+ const data = req.body;
+ queries.register(data, (err, results) => {
+  if (err) {
+   res.send(err);
+  } else {
+   res.json(results);
+   console.log(results);
+  };
+ });
+});
+
+router.post('/test', (req,res) => {
+ const data = req.body;
+ res.json( {'message': 'wena'} );
+ console.log(data);
+})
 
 module.exports = router;
