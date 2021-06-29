b5439d816f0f68c5590f07b17d07630304a604d6
Hector Saravia <hector.saravia@mail.udp.cl>
2021-06-28 23:24:55 -0400

    Actualizado archivo principal backend: -Añadida dependencia hacia el archivo routes.js -Añadido cors middleware -Añadidos comentarios en el archivo

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
-//Requerimiento de ExpressJS. No pasar a ES6
+//Requerimientos para backend. No pasar a ES6
 const express = require('express');
+const cors = require('cors');
+
+//Importar rutas
+const routes = require('./routes');
 
 //Se define app como la variable de ExpressJS
 const app = express();
+
+//Uso de cors
+app.use(cors());
 
 //Uso del puerto 3000
 const port = 3000;
@@ -10,6 +17,8 @@ const port = 3000;
 //Se indica que se aceptan JSON y x-www-form-urlencoded
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use('/api', routes);
 
 app.get('/', (req,res) => {
  res.json({'message': 'ok'})
