23323b5b35a0e200c7518fa63de3c5089ca4cee5
Hector Saravia <hector.saravia@mail.udp.cl>
2021-06-29 01:13:09 -0400

    Añadida función para registros y encriptado bcrypt

--- backend/database/queries.js
+++ backend/database/queries.js
@@ -1,4 +1,7 @@
 const db = require('./config');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const getTables = (result) => {
  db.query(`show tables;`, (err, results) => {
@@ -11,6 +14,31 @@ const getTables = (result) => {
  });
 }
 
+const register = (data, result) => {
+ password = data.password;
+ bcrypt.hash(password, saltRounds)
+ .then(function (hashPassword) {
+  password = hashPassword;
+ }).catch(function (err) {
+  console.log(err);
+  result(err,null);
+ });
+ db.query(`INSERT INTO users(email,password,rut,name,rol) VALUES(?,?,?,?,?)`,
+  [data.email, password, data.rut, data.name, data.rol],
+  (err, results) => {
+   if (err) {
+    console.log(err);
+    result(err,null);
+   } else {
+    result(null,results);
+   };
+  }); 
+}
+
+
+
+
 module.exports = {
  getTables,
+ register
 }
