86f73310dbb32e60b6b7ab026aded27b2d52d0f8
Hector Saravia <hector.saravia@mail.udp.cl>
2021-07-12 17:43:32 -0400

    Arreglado bug para mostrar cursos correctamente

--- backend/database/mostrar_cursos.js
+++ backend/database/mostrar_cursos.js
@@ -3,25 +3,25 @@ const db = require('./config');
 //función asíncrona para mostrar los cursos
 module.exports = async function mostrar_cursos (data) {
  
- //variable de resultado que será el retorno
- let result;
+    //variable de resultado que será el retorno
+    let result;
 
- //query que realizará la búsqueda de los cursos en la carrera
- db.promise().query(`SELECT id,name FROM courses WHERE id_career = ?;`,
- [data.id_career]) 
- .then( ([rows,fields]) => {
-   
-  //si no hay cursos, se retorna un false, en otro caso se realiza el retorno
-  if (rows.lenght === 0) result = false;
-  else result = rows
- 
- })
+    //query que realizará la búsqueda de los cursos en la carrera
+    await db.promise().query(`SELECT id,name FROM courses WHERE id_career = ?;`,
+    [data.id_career]) 
+    .then( ([rows,fields]) => {
+        
+        //si no hay cursos, se retorna un false, en otro caso se realiza el retorno
+        if (rows.lenght === 0) result = false;
+        else result = JSON.stringify(rows);
+    
+    })
 
- //en caso de ocurrir un error, este será el retorno
- .catch((error) => {
-  result = error;
- })
+    //en caso de ocurrir un error, este será el retorno
+    .catch((error) => {
+    result = error;
+    })
 
- console.log(`cursos obtenidos: ${result}`)
- return result;
+    console.log(`cursos obtenidos: ${result}`)
+    return result;
 }
