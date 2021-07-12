955fc0d77edc94e91a1fa36c336454fc57ee546c
Hector Saravia <hector.saravia@mail.udp.cl>
2021-07-12 17:44:05 -0400

    Añadida función para mostrar preguntas de usuario

---
+++ backend/database/mostrar_preguntas_usuario.js
@@ -0,0 +1,32 @@
+const db = require('./config');
+
+//función asíncrona para desplegar todas las preguntas de un usuario
+module.exports = async function mostrar_preguntas_usuario (data) {
+
+    //declaración de variable result que será el retorno
+    let result;
+
+    //se ejecuta la query para obtener todas las preguntass
+    await db.promise().query(`SELECT question,description,votes,state FROM questions,users
+                            FROM users,questions WHERE questions.email_user=? `, [data.email])
+    .then( ([rows,fields]) => {
+
+        //si el resultado es 0 entonces no hay cursos, en otro caso se retornan
+        if(rows.length === 0) result = false;
+        else{
+            result = rows;
+
+            for(let row of rows) {
+                console.log(row);
+            }
+        }
+    })
+    
+    //si ocurre un error, el resultado será el error
+    .catch((error) => {
+        result = error;
+    })
+
+    //acuso de resultado de la operación y retorno del resultado
+    return result;
+}
