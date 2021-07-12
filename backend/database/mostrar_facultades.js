f0625707eaf3568aa47e30197da22c521a08ed2f
Hector Saravia <hector.saravia@mail.udp.cl>
2021-07-12 17:43:52 -0400

    Arreglado error para mostrar facultades

--- backend/database/mostrar_facultades.js
+++ backend/database/mostrar_facultades.js
@@ -3,25 +3,30 @@ const db = require('./config');
 //función para desplegar las facultades
 module.exports = async function mostrar_facultades () {
  
- //declaración de variable result que será el retorno
- let result;
+    //declaración de variable result que será el retorno
+    let result;
 
- //se ejecuta la query para obtener todas las facultades
- db.promise().query(`SELECT id,name FROM school;`)
- .then( ([rows,fields])  => {
+    //se ejecuta la query para obtener todas las facultades
+    await db.promise().query(`SELECT id,name FROM school;`)
+    .then( ([rows,fields])  => {
 
-  //si el resultado es 0 entonces no hay facultades, en otro caso se retornan
-  if(rows.lenght === 0) return false;
-  else result = rows;
+        //si el resultado es 0 entonces no hay facultades, en otro caso se retornan
+        if(rows.lenght === 0) return false;
+        else {
+            result = rows;
+            for (let row of rows) {
+                console.log(row.name);
+            }
+        };
  
- })
+    })
  
- //si ocurre un error, el resultado será el error
- .catch( (error) => {
-  result = error;
- })
+    //si ocurre un error, el resultado será el error
+    .catch( (error) => {
+        result = error;
+    })
 
- //acuso de resultado de la operación y retorno del resultado
- console.log(`facultados a mostrar: ${result}`);
- return result;
+    //acuso de resultado de la operación y retorno del resultado
+    console.log(`facultados a mostrar: ${result}`);
+    return result;
 }
